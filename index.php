c258acb4536d3697e71fd2ad8b15c38ba4df63c1
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:21:25 +0100

    exercice_php_P3_1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  $i = 0;
+  while ($i <= 10) {
+      echo $i++;  
+  }
+ ?>
+</body>
+</html>
